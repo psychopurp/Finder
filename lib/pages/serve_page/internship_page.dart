2ce2e03cab6ee6a1632a20949fd26d29d61be004
Tobias Yin <yrt1999@163.com>
2019-10-23 13:05:32 +0800

    Update.

--- lib/pages/serve_page/internship_page.dart
+++ lib/pages/serve_page/internship_page.dart
@@ -16,8 +16,21 @@ class InternshipPage extends StatefulWidget {
 }
 
 class _InternshipPageState extends State<InternshipPage> {
-  List<InternshipItem> bannerData = [];
-  List<InternshipItem> data = [InternshipItem(id: 0)];
+  List<InternshipItem> _bannerData = [];
+  List<InternshipItem> _data = [];
+  List<InternshipBigType> _bigTypes = [];
+  Map<InternshipBigType, List<InternshipSmallType>> _smallTypes = {};
+  InternshipBigType _nowBigType;
+  List<InternshipSmallType> _nowSmallTypes;
+  int _nowPage = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecommend();
+    getBigTypes();
+    getInternships();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,35 +50,59 @@ class _InternshipPageState extends State<InternshipPage> {
               },
             ),
           ),
-          body
+          Expanded(
+            flex: 1,
+            child: body,
+          )
         ],
       ),
     );
   }
 
-  Widget get body => ListView.builder(
+  Widget get body {
+    Widget child;
+    if (_bannerData.length == 0 && _data.length == 0) {
+      child = Center(
+        child: Text("暂时没有数据"),
+      );
+    } else if (_bannerData.length == 0) {
+      child = ListView.builder(
+        physics: AlwaysScrollableScrollPhysics(),
+        itemBuilder: (context, index) => getItem(index),
+        itemCount: _data.length,
+        padding: EdgeInsets.only(top: 5),
+      );
+    } else {
+      child = ListView.builder(
+        physics: AlwaysScrollableScrollPhysics(),
         itemBuilder: (context, index) {
           if (index == 0) {
             return banner;
           }
           return getItem(index - 1);
         },
-        itemCount: data.length + 1,
+        itemCount: _data.length + 1,
+        padding: EdgeInsets.only(top: 5),
       );
+    }
+    return RefreshIndicator(
+      child: child,
+      onRefresh: () async {},
+    );
+  }
 
   get banner {
     return Container(
-      margin: EdgeInsets.only(top: 0, bottom: 10),
       padding: EdgeInsets.only(bottom: 15.0),
       height: 250,
-      width: 400,
-      color: Colors.white,
+      width: double.infinity,
+      color: Colors.transparent,
       child: Swiper(
-        itemCount: bannerData.length,
-        autoplay: bannerData.length > 1,
+        itemCount: _bannerData.length,
+        autoplay: _bannerData.length > 1,
         onTap: (index) {
           Navigator.pushNamed(context, Routes.heSaysDetail,
-              arguments: bannerData[index]);
+              arguments: _bannerData[index]);
         },
         pagination: SwiperPagination(
           margin: const EdgeInsets.only(
@@ -79,12 +116,12 @@ class _InternshipPageState extends State<InternshipPage> {
           ),
         ),
         itemBuilder: (context, index) {
-          InternshipItem item = bannerData[index];
+          InternshipItem item = _bannerData[index];
           var image = CachedNetworkImage(
             imageUrl: item.image,
             imageBuilder: (context, imageProvider) => Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(3)),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
                 image: DecorationImage(
                   image: imageProvider,
                   fit: BoxFit.cover,
@@ -94,7 +131,7 @@ class _InternshipPageState extends State<InternshipPage> {
           );
           return Padding(
             padding: EdgeInsets.only(bottom: 25, left: 10, right: 10),
-            child: bannerData.length > 1
+            child: _bannerData.length > 1
                 ? Hero(tag: item.id, child: image)
                 : image,
           );
@@ -104,28 +141,109 @@ class _InternshipPageState extends State<InternshipPage> {
   }
 
   Widget getItem(int index) {
-    return null;
+    return Container(
+      margin: EdgeInsets.only(bottom: 10, left: 5, right: 5),
+      width: double.infinity,
+      height: 150,
+      color: Colors.red,
+    );
   }
 
-  Future<void> getLeadHeSheSays({bool delay: false}) async {
-    if (delay) {
-      await Future.delayed(Duration(milliseconds: 100));
-    }
+  Future<void> getRecommend() async {
+    String url = 'get_recommend_internships/';
     try {
       Dio dio = ApiClient.dio;
-      Response response = await dio.get('get_lead_he_she_say/');
+      Response response = await dio.get(url);
       Map<String, dynamic> result = response.data;
       if (result["status"]) {
         setState(() {
-          bannerData = List.generate(result["data"].length,
+          _bannerData = List<InternshipItem>.generate(result["data"].length,
+              (index) => InternshipItem.recommend(result["data"][index]));
+        });
+      }
+    } on DioError catch (e) {
+      print(e);
+      print(url);
+      setState(() {
+        _bannerData = [];
+      });
+    }
+  }
+
+  Future<void> getBigTypes() async {
+    String url = 'get_internship_big_types/';
+    try {
+      Dio dio = ApiClient.dio;
+      Response response = await dio.get(url);
+      Map<String, dynamic> result = response.data;
+      if (result["status"]) {
+        setState(() {
+          _bigTypes = List<InternshipBigType>.generate(result["data"].length,
+              (index) => InternshipBigType.fromJson(result["data"][index]));
+        });
+      }
+    } on DioError catch (e) {
+      print(e);
+      print(url);
+      setState(() {
+        if (_bigTypes == null) {
+          _bigTypes = [];
+        }
+      });
+    }
+  }
+
+  Future<void> getSmallTypes(InternshipBigType bigType) async {
+    if (_smallTypes.containsKey(bigType)) {
+      return;
+    }
+    String url = 'get_internship_small_types/';
+    try {
+      Dio dio = ApiClient.dio;
+      Response response =
+          await dio.get(url, queryParameters: {'big_type_id': bigType.id});
+      Map<String, dynamic> result = response.data;
+      if (result["status"]) {
+        setState(() {
+          List<InternshipSmallType> smallTypes =
+              List<InternshipSmallType>.generate(
+                  result["data"].length,
+                  (index) =>
+                      InternshipSmallType.fromJson(result["data"][index]));
+          _smallTypes[bigType] = smallTypes;
+        });
+      }
+    } on DioError catch (e) {
+      print(e);
+      print(url);
+      setState(() {
+        if (_smallTypes.containsKey(bigType)) {
+          _smallTypes.remove(bigType);
+        }
+      });
+    }
+  }
+
+  Future<void> getInternships() async {
+    String url = 'get_internships/';
+    try {
+      Dio dio = ApiClient.dio;
+      Map<String, dynamic> query = {'page': _nowPage};
+      if (_nowSmallTypes != null && _nowSmallTypes.length != 0) {
+        query['small_type_ids'] = List<int>.generate(
+            _nowSmallTypes.length, (index) => _nowSmallTypes[index].id);
+      }
+      Response response = await dio.get(url, queryParameters: query);
+      Map<String, dynamic> result = response.data;
+      if (result["status"]) {
+        setState(() {
+          _data = List<InternshipItem>.generate(result["data"].length,
               (index) => InternshipItem.fromJson(result["data"][index]));
         });
       }
     } on DioError catch (e) {
       print(e);
-      setState(() {
-        bannerData = [];
-      });
+      print(url);
     }
   }
 }
@@ -249,29 +367,30 @@ class _InternshipPageHeaderState extends State<InternshipPageHeader>
             ),
           ),
           Positioned(
-              right: _positionAnimation.value,
-              child: Opacity(
-                opacity: 1 - _opacityAnimation.value,
-                child: Container(
-                  width: inputWidth,
-                  child: TextField(
-                    focusNode: _focusNode,
-                    maxLines: 1,
-                    cursorColor: ActionColor,
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 7, horizontal: 10),
-                      filled: true,
-                      fillColor: Color.fromARGB(255, 245, 241, 241),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none,
-                      ),
+            right: _positionAnimation.value,
+            child: Opacity(
+              opacity: 1 - _opacityAnimation.value,
+              child: Container(
+                width: inputWidth,
+                child: TextField(
+                  focusNode: _focusNode,
+                  maxLines: 1,
+                  cursorColor: ActionColor,
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 7, horizontal: 10),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 245, 241, 241),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
                     ),
                   ),
                 ),
-              )),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -280,22 +399,48 @@ class _InternshipPageHeaderState extends State<InternshipPageHeader>
 
 class InternshipItem {
   InternshipItem(
-      {this.id, this.image, this.company, this.types, this.salaryRange});
+      {this.id,
+      this.image,
+      this.company,
+      this.types,
+      this.salaryRange,
+      this.tags,
+      this.title});
 
   factory InternshipItem.fromJson(Map<String, dynamic> map) {
     return InternshipItem(
-        id: map['id'],
-        image: Avatar.getImageUrl(map['image']),
-        company: CompanyItem.fromJson(map['company']),
-        types: List<InternshipType>.generate(map['types'].length,
-            (index) => InternshipType.fromJson(map['types'][index])),
-        salaryRange: map['salary_range']);
+      id: map['id'],
+      company: CompanyItem.fromJson(map['company']),
+      types: List<InternshipSmallType>.generate((map['types'] ?? []).length,
+          (index) => InternshipSmallType.fromJson(map['types'][index])),
+      tags: List<InternshipTag>.generate((map['tags'] ?? []).length,
+          (index) => InternshipTag.fromJson(map['tags'][index])),
+      salaryRange: map['salary_range'],
+      title: map['title'],
+    );
+  }
+
+  factory InternshipItem.recommend(Map<String, dynamic> json) {
+    Map<String, dynamic> map = json["internship"];
+    return InternshipItem(
+      id: map['id'],
+      image: Avatar.getImageUrl(json['image']),
+      company: CompanyItem.fromJson(map['company']),
+      types: List<InternshipSmallType>.generate((map['types'] ?? []).length,
+          (index) => InternshipSmallType.fromJson(map['types'][index])),
+      tags: List<InternshipTag>.generate((map['tags'] ?? []).length,
+          (index) => InternshipTag.fromJson(map['tags'][index])),
+      salaryRange: map['salary_range'],
+      title: map['title'],
+    );
   }
 
   final int id;
+  final String title;
   final String image;
   final CompanyItem company;
-  final List<InternshipType> types;
+  final List<InternshipSmallType> types;
+  final List<InternshipTag> tags;
   final String salaryRange;
 }
 
@@ -303,6 +448,7 @@ class CompanyItem {
   CompanyItem({this.id, this.name, this.image});
 
   factory CompanyItem.fromJson(Map<String, dynamic> map) {
+    if (map == null) return null;
     return CompanyItem(
         id: map["id"],
         name: map["name"],
@@ -317,8 +463,51 @@ class CompanyItem {
 class InternshipType {
   InternshipType({this.id, this.name});
 
-  factory InternshipType.fromJson(Map<String, dynamic> map) {
-    return InternshipType(
+  final int id;
+  final String name;
+
+  @override
+  int get hashCode {
+    if (runtimeType == InternshipSmallType) {
+      return 1000000000 + id;
+    } else {
+      return 2000000000 + id;
+    }
+  }
+
+  @override
+  bool operator ==(other) {
+    return other.runtimeType == runtimeType && other.id == this.id;
+  }
+}
+
+class InternshipSmallType extends InternshipType {
+  InternshipSmallType({int id, String name}) : super(id: id, name: name);
+
+  factory InternshipSmallType.fromJson(Map<String, dynamic> map) {
+    return InternshipSmallType(
+      id: map["id"],
+      name: map["name"],
+    );
+  }
+}
+
+class InternshipBigType extends InternshipType {
+  InternshipBigType({int id, String name}) : super(id: id, name: name);
+
+  factory InternshipBigType.fromJson(Map<String, dynamic> map) {
+    return InternshipBigType(
+      id: map["id"],
+      name: map["name"],
+    );
+  }
+}
+
+class InternshipTag {
+  InternshipTag({this.id, this.name});
+
+  factory InternshipTag.fromJson(Map<String, dynamic> map) {
+    return InternshipTag(
       id: map["id"],
       name: map["name"],
     );
