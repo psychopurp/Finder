a2c71f4416e116f1aaadef62f32c1d80cf62d489
尹瑞涛 <yrt1999@163.com>
2019-10-23 11:28:48 +0800

    Update.

--- lib/pages/serve_page/internship_page.dart
+++ lib/pages/serve_page/internship_page.dart
@@ -1,9 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:finder/config/api_client.dart';
+import 'package:finder/plugin/avatar.dart';
 import 'package:finder/public.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
+const Color ActionColorActive = Color(0xFFEC7C6D);
+const Color PageBackgroundColor = Color.fromARGB(255, 233, 229, 228);
 const Color ActionColor = Color(0xFFDB6B5C);
 
 class InternshipPage extends StatefulWidget {
@@ -13,11 +17,10 @@ class InternshipPage extends StatefulWidget {
 
 class _InternshipPageState extends State<InternshipPage> {
   List<InternshipItem> bannerData = [];
+  List<InternshipItem> data = [InternshipItem(id: 0)];
 
   @override
   Widget build(BuildContext context) {
-    var appBarColor = Color.fromARGB(255, 95, 95, 95);
-    var appBarIconColor = Color.fromARGB(255, 155, 155, 155);
     Future<void>.delayed(Duration(milliseconds: 200), () {
       SystemChrome.setSystemUIOverlayStyle(
           SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark));
@@ -28,13 +31,80 @@ class _InternshipPageState extends State<InternshipPage> {
           Container(
             padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
             color: Colors.white,
-            child: InternshipPageHeader(onSearch: (text){
-              print(text);
-            },),
+            child: InternshipPageHeader(
+              onSearch: (text) {
+                print(text);
+              },
+            ),
           ),
+          body
         ],
       ),
     );
+  }
+
+  Widget get body => ListView.builder(
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return banner;
+          }
+          return getItem(index - 1);
+        },
+        itemCount: data.length + 1,
+      );
+
+  get banner {
+    return Container(
+      margin: EdgeInsets.only(top: 0, bottom: 10),
+      padding: EdgeInsets.only(bottom: 15.0),
+      height: 250,
+      width: 400,
+      color: Colors.white,
+      child: Swiper(
+        itemCount: bannerData.length,
+        autoplay: bannerData.length > 1,
+        onTap: (index) {
+          Navigator.pushNamed(context, Routes.heSaysDetail,
+              arguments: bannerData[index]);
+        },
+        pagination: SwiperPagination(
+          margin: const EdgeInsets.only(
+              top: 10.0, left: 10.0, right: 10.0, bottom: 5.0),
+          alignment: Alignment.bottomCenter,
+          builder: DotSwiperPaginationBuilder(
+            size: 6,
+            activeSize: 7,
+            activeColor: ActionColorActive,
+            color: ActionColor,
+          ),
+        ),
+        itemBuilder: (context, index) {
+          InternshipItem item = bannerData[index];
+          var image = CachedNetworkImage(
+            imageUrl: item.image,
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(3)),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          );
+          return Padding(
+            padding: EdgeInsets.only(bottom: 25, left: 10, right: 10),
+            child: bannerData.length > 1
+                ? Hero(tag: item.id, child: image)
+                : image,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget getItem(int index) {
+    return null;
   }
 
   Future<void> getLeadHeSheSays({bool delay: false}) async {
@@ -60,17 +130,12 @@ class _InternshipPageState extends State<InternshipPage> {
   }
 }
 
-class InternshipItem {
-  factory InternshipItem.fromJson(Map<String, dynamic> map) {
-    return null;
-  }
-}
-
 typedef SearchCallBack = void Function(String text);
 
 class InternshipPageHeader extends StatefulWidget
     implements PreferredSizeWidget {
   InternshipPageHeader({this.onSearch});
+
   final SearchCallBack onSearch;
   final double height = 56;
 
@@ -169,14 +234,14 @@ class _InternshipPageHeaderState extends State<InternshipPageHeader>
                 color: appBarIconColor,
               ),
               onPressed: () {
-                if(!open){
+                if (!open) {
                   _animationController.forward();
                   FocusScope.of(context).requestFocus(_focusNode);
-                }else{
+                } else {
                   _animationController.reverse();
                   String text = _searchController.text;
                   _searchController.clear();
-                  (widget.onSearch ?? (text){})(text);
+                  (widget.onSearch ?? (text) {})(text);
                   _focusNode.unfocus();
                 }
                 open = !open;
@@ -195,7 +260,8 @@ class _InternshipPageHeaderState extends State<InternshipPageHeader>
                     cursorColor: ActionColor,
                     controller: _searchController,
                     decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(vertical: 7, horizontal: 10),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 7, horizontal: 10),
                       filled: true,
                       fillColor: Color.fromARGB(255, 245, 241, 241),
                       border: OutlineInputBorder(
@@ -205,10 +271,59 @@ class _InternshipPageHeaderState extends State<InternshipPageHeader>
                     ),
                   ),
                 ),
-              )
-          ),
+              )),
         ],
       ),
     );
   }
+}
+
+class InternshipItem {
+  InternshipItem(
+      {this.id, this.image, this.company, this.types, this.salaryRange});
+
+  factory InternshipItem.fromJson(Map<String, dynamic> map) {
+    return InternshipItem(
+        id: map['id'],
+        image: Avatar.getImageUrl(map['image']),
+        company: CompanyItem.fromJson(map['company']),
+        types: List<InternshipType>.generate(map['types'].length,
+            (index) => InternshipType.fromJson(map['types'][index])),
+        salaryRange: map['salary_range']);
+  }
+
+  final int id;
+  final String image;
+  final CompanyItem company;
+  final List<InternshipType> types;
+  final String salaryRange;
+}
+
+class CompanyItem {
+  CompanyItem({this.id, this.name, this.image});
+
+  factory CompanyItem.fromJson(Map<String, dynamic> map) {
+    return CompanyItem(
+        id: map["id"],
+        name: map["name"],
+        image: Avatar.getImageUrl(map["image"]));
+  }
+
+  final int id;
+  final String name;
+  final String image;
+}
+
+class InternshipType {
+  InternshipType({this.id, this.name});
+
+  factory InternshipType.fromJson(Map<String, dynamic> map) {
+    return InternshipType(
+      id: map["id"],
+      name: map["name"],
+    );
+  }
+
+  final int id;
+  final String name;
 }
