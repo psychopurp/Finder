7f08ace881f3fddf4049ba600f3876a7b09175b4
Tobias Yin <yrt1999@163.com>
2019-11-07 03:15:04 +0800

    Bug fix.

--- lib/pages/web_view.dart
+++ lib/pages/web_view.dart
@@ -15,7 +15,7 @@ class WebViewPage extends StatelessWidget {
       withZoom: true,
       withJavascript: true,
       withLocalStorage: true,
-      hidden: true,
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
       initialChild:
           Container(height: double.infinity, child: FinderDialog.showLoading()),
     );
