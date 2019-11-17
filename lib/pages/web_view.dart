6fd6d02d6bc5d03c14d125e9b1682b84f1a20cee
尹瑞涛 <yrt1999@163.com>
2019-11-17 16:43:12 +0800

    Update Better text and web view.

--- lib/pages/web_view.dart
+++ lib/pages/web_view.dart
@@ -1,23 +1,152 @@
 import 'package:finder/public.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class WebViewPage extends StatelessWidget {
+const Color ActionColor = Color(0xFFDB6B5C);
+
+class WebViewPage extends StatefulWidget {
   final String url;
   final String title;
+
   WebViewPage({@required this.url, this.title});
+
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  FlutterWebviewPlugin webController;
+  Map<String, Map<String, dynamic>> menuItem;
+  String nowUrl;
+  bool error = false;
+
+  @override
+  void initState() {
+    super.initState();
+    nowUrl = widget.url;
+    webController = FlutterWebviewPlugin();
+    webController.onHttpError.listen((detail) async {
+      String url = detail.url;
+      if (!url.startsWith("http")) {
+        if (await canLaunch(url)) {
+          launch(url);
+        }
+        webController.goBack();
+        return;
+      }
+      if (url == nowUrl || !url.startsWith("https")) {
+        setState(() {
+          error = true;
+        });
+      }
+    });
+    webController.onUrlChanged.listen((url) async {
+      nowUrl = url;
+    });
+    menuItem = {
+      "浏览器打开": {"handle": _handleExplore, "icon": Icons.explore},
+    };
+  }
+
+  _handleExplore() async {
+    if (await canLaunch(nowUrl)) {
+      launch(nowUrl);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    webController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('=================url====>$url');
-    return WebviewScaffold(
-      url: url,
-      appBar: AppBar(),
-      withZoom: true,
-      withJavascript: true,
-      withLocalStorage: true,
-      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-      initialChild:
-          Container(height: double.infinity, child: FinderDialog.showLoading()),
+//    print('=================url====>${widget.url}');
+    AppBar appBar = AppBar(
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 20),
+          child: PopupMenuButton(
+            padding: EdgeInsets.symmetric(horizontal: 18),
+            color: Colors.white,
+            itemBuilder: (context) {
+              List<PopupMenuItem<String>> list = [];
+              menuItem.forEach(
+                (key, value) {
+                  list.add(
+                    PopupMenuItem<String>(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(menuItem[key]["icon"]),
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              key,
+                              textAlign: TextAlign.right,
+                            ),
+                          ),
+                        ],
+                      ),
+                      value: key,
+                    ),
+                  );
+                },
+              );
+              return list;
+            },
+            onSelected: (key) {
+              menuItem[key]["handle"]();
+            },
+            child: Icon(
+              Icons.more_horiz,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        )
+      ],
     );
+    return !error
+        ? WebviewScaffold(
+            url: widget.url,
+            appBar: appBar,
+            withZoom: true,
+            withJavascript: true,
+            withLocalStorage: true,
+            debuggingEnabled: false,
+            userAgent:
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/72.0.3626.101 Mobile/15E148 Safari/605.1",
+            initialChild: Container(
+                height: double.infinity, child: FinderDialog.showLoading()),
+          )
+        : Scaffold(
+            appBar: appBar,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("网页加载失败, 请尝试使用系统浏览器打开. "),
+                  Padding(padding: EdgeInsets.all(30),),
+                  FlatButton(
+                    child: Text("使用浏览器打开"),
+                    color: Theme.of(context).primaryColor,
+                    highlightColor: Theme.of(context).primaryColor.withOpacity(0.7),
+                    colorBrightness: Brightness.dark,
+                    splashColor: Theme.of(context).primaryColor.withOpacity(0.9),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    onPressed: () {
+                      _handleExplore();
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
   }
 }
--- lib/plugin/better_text.dart
+++ lib/plugin/better_text.dart
@@ -1,26 +1,29 @@
 import 'package:bot_toast/bot_toast.dart';
-import 'package:clipboard_manager/clipboard_manager.dart';
+import 'package:finder/routers/application.dart';
+import 'package:finder/routers/routes.dart';
+import 'package:flutter/rendering.dart';
+import 'package:extended_text/extended_text.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class BetterText extends StatefulWidget {
   BetterText(
-      this.text, {
-        Key key,
-        this.urlStyle,
-        this.style,
-        this.strutStyle,
-        this.textAlign,
-        this.textDirection,
-        this.locale,
-        this.softWrap,
-        this.overflow,
-        this.textScaleFactor,
-        this.maxLines,
-        this.semanticsLabel,
-        this.textWidthBasis,
-      }) : super(key: key);
+    this.text, {
+    Key key,
+    this.urlStyle,
+    this.style,
+    this.strutStyle,
+    this.textAlign,
+    this.textDirection,
+    this.locale,
+    this.softWrap,
+    this.overflow,
+    this.textScaleFactor,
+    this.maxLines,
+    this.semanticsLabel,
+    this.textWidthBasis,
+  }) : super(key: key);
   final TextStyle style;
   final StrutStyle strutStyle;
   final TextAlign textAlign;
@@ -41,7 +44,7 @@ class BetterText extends StatefulWidget {
 
 class _BetterTextState extends State<BetterText> {
   static final RegExp urlReg =
-  RegExp(r"https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]");
+      RegExp(r"https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]");
   List<_TextItem> texts = [];
 
   @override
@@ -51,6 +54,10 @@ class _BetterTextState extends State<BetterText> {
   }
 
   void splitText(String text) {
+    if (text.length == 1) {
+      texts.add(_TextItem(text, false));
+      return;
+    }
     var matches = urlReg.allMatches(text);
     int start = 0;
     for (var match in matches) {
@@ -65,59 +72,50 @@ class _BetterTextState extends State<BetterText> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      child: Text.rich(
-        TextSpan(
-            children: List<InlineSpan>.generate(
-                texts.length,
-                    (index) => !texts[index].type
-                    ? TextSpan(text: texts[index].text)
-                    : TextSpan(
-                    text: texts[index].text,
-                    style: widget.urlStyle ??
-                        const TextStyle(color: Colors.blue),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () async {
-                        if (await canLaunch(texts[index].text)) {
-                          launch(texts[index].text);
-                        }
-                      }))),
-        style: widget.style,
-        textAlign: widget.textAlign,
-        maxLines: widget.maxLines,
-        overflow: widget.overflow,
-        semanticsLabel: widget.semanticsLabel,
-        locale: widget.locale,
-        softWrap: widget.softWrap,
-        strutStyle: widget.strutStyle,
-        textDirection: widget.textDirection,
-        textScaleFactor: widget.textScaleFactor,
-        textWidthBasis: widget.textWidthBasis,
-      ),
-      onLongPress: () {
-        ClipboardManager.copyToClipBoard(widget.text).then((result) {
-          if (result) {
-            BotToast.showText(text: "复制文本成功了哟~", align: Alignment(0, 0.8));
-          } else {
-            BotToast.showText(
-                text: "复制失败, 请检查权限再试试吧~", align: Alignment(0, 0.8));
-          }
-        });
-      },
+    return ExtendedText.rich(
+      TextSpan(
+          children: List<InlineSpan>.generate(
+              texts.length,
+              (index) => !texts[index].type
+                  ? TextSpan(text: texts[index].text)
+                  : TextSpan(
+                      text: texts[index].text,
+                      style: widget.urlStyle ??
+                          const TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () async {
+                          String url = texts[index].text;
+                          url = Uri.encodeComponent(url);
+                          Application.router
+                              .navigateTo(context, "${Routes.webViewPage}?url=$url");
+//                          if (await canLaunch(texts[index].text)) {
+//                            launch(texts[index].text);
+//                          }
+                        }))),
+      textSelectionControls: MyExtendedMaterialTextSelectionControls(),
+      selectionEnabled: true,
+      style: widget.style,
+      textAlign: widget.textAlign,
+      maxLines: widget.maxLines,
+      overflow: widget.overflow,
+      semanticsLabel: widget.semanticsLabel,
+      locale: widget.locale,
+      softWrap: widget.softWrap,
+      strutStyle: widget.strutStyle,
+      textDirection: widget.textDirection,
+      textScaleFactor: widget.textScaleFactor,
+      textWidthBasis: widget.textWidthBasis,
     );
   }
 
   @override
   void didUpdateWidget(BetterText oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if(oldWidget.text != widget.text){
+    if (oldWidget.text != widget.text) {
       texts = [];
       splitText(widget.text);
     }
   }
-
-
 }
 
 class _TextItem {
@@ -125,4 +123,113 @@ class _TextItem {
 
   final String text;
   final bool type;
+}
+
+const double _kToolbarScreenPadding = 8.0;
+const double _kToolbarHeight = 44.0;
+
+class MyExtendedMaterialTextSelectionControls
+    extends MaterialExtendedTextSelectionControls {
+  MyExtendedMaterialTextSelectionControls();
+
+  @override
+  Widget buildToolbar(
+    BuildContext context,
+    Rect globalEditableRegion,
+    double textLineHeight,
+    Offset position,
+    List<TextSelectionPoint> endpoints,
+    TextSelectionDelegate delegate,
+  ) {
+    assert(debugCheckHasMediaQuery(context));
+    assert(debugCheckHasMaterialLocalizations(context));
+
+    final TextSelectionPoint startTextSelectionPoint = endpoints[0];
+    final TextSelectionPoint endTextSelectionPoint =
+        (endpoints.length > 1) ? endpoints[1] : null;
+    final double x = (endTextSelectionPoint == null)
+        ? startTextSelectionPoint.point.dx
+        : (startTextSelectionPoint.point.dx + endTextSelectionPoint.point.dx) /
+            2.0;
+    final double availableHeight = globalEditableRegion.top -
+        MediaQuery.of(context).padding.top -
+        _kToolbarScreenPadding;
+    final double y = (availableHeight < _kToolbarHeight)
+        ? startTextSelectionPoint.point.dy +
+            globalEditableRegion.height +
+            _kToolbarHeight +
+            _kToolbarScreenPadding
+        : startTextSelectionPoint.point.dy - textLineHeight * 2.0;
+    final Offset preciseMidpoint = Offset(x, y);
+
+    return ConstrainedBox(
+      constraints: BoxConstraints.tight(globalEditableRegion.size),
+      child: CustomSingleChildLayout(
+        delegate: MaterialExtendedTextSelectionToolbarLayout(
+          MediaQuery.of(context).size,
+          globalEditableRegion,
+          preciseMidpoint,
+        ),
+        child: _TextSelectionToolbar(
+          handleCut: canCut(delegate) ? () => handleCut(delegate) : null,
+          handleCopy: canCopy(delegate)
+              ? () => (delegate) {
+                    handleCopy(delegate);
+                    BotToast.showText(text: "复制成功", align: Alignment(0, 0.8));
+                  }(delegate)
+              : null,
+          handlePaste: canPaste(delegate) ? () => handlePaste(delegate) : null,
+          handleSelectAll:
+              canSelectAll(delegate) ? () => handleSelectAll(delegate) : null,
+        ),
+      ),
+    );
+  }
+}
+
+/// Manages a copy/paste text selection toolbar.
+class _TextSelectionToolbar extends StatelessWidget {
+  const _TextSelectionToolbar({
+    Key key,
+    this.handleCopy,
+    this.handleSelectAll,
+    this.handleCut,
+    this.handlePaste,
+  }) : super(key: key);
+
+  final VoidCallback handleCut;
+  final VoidCallback handleCopy;
+  final VoidCallback handlePaste;
+  final VoidCallback handleSelectAll;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> items = <Widget>[];
+
+    if (handleCut != null) items.add(createButton("剪切", handleCut));
+    if (handleCopy != null) items.add(createButton("复制", handleCopy));
+    if (handlePaste != null) items.add(createButton("粘贴", handlePaste));
+    if (handleSelectAll != null) items.add(createButton("全选", handleSelectAll));
+    // If there is no option available, build an empty widget.
+    if (items.isEmpty) {
+      return Container(width: 0.0, height: 0.0);
+    }
+
+    return Material(
+      elevation: 1.0,
+      child: Wrap(children: items),
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    );
+  }
+
+  Widget createButton(String text, VoidCallback onTrigger) {
+    return Container(
+      child: FlatButton(
+        child: Text(text),
+        onPressed: onTrigger,
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+    );
+  }
 }
